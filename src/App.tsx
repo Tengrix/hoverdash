dfcd4485def7613ff9b76e7758f12bf357f08c57
Tengrix <damir861@gmail.com>
2021-09-29 23:35:14 -0400

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const colors = ['#0066CC','#00CC66','#6600CC','#FF9933','#66FF66', '#FF3333','#FFFF33', '#FFFF99']
+    const newArr = []
+    const numOfDiv = 500
+
+    const squareHandlerOn = (e:React.MouseEvent<HTMLDivElement>) => {
+        let color = colors[Math.floor(Math.random()*colors.length)]
+        e.currentTarget.style.backgroundColor = `${color}`
+        e.currentTarget.style.boxShadow = `0 0 2px ${color}, , 0 0 10px ${color}`
+    }
+    const squareHandlerLeave = (e:React.MouseEvent<HTMLDivElement>) => {
+        e.currentTarget.style.backgroundColor = ''
+        e.currentTarget.style.boxShadow = `0 0 2px #000`
+    }
+    for(let i = 0; i < numOfDiv;i++){
+        newArr.push(<div onMouseLeave={squareHandlerLeave} onMouseOver={squareHandlerOn} className={'square'}/>)
+    }
+    return (
+        <div className="App">
+            <div className={'container'}>
+
+                {newArr.map(el=><div>{el}</div>)}
+            </div>
+        </div>
+    );
 }
 
 export default App;
